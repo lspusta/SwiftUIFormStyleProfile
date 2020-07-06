1f2e8672c7ff69e67a17f8bb6e7dcff862a5eb76
Martin Spusta <lucas.spusta@gmail.com>
2020-07-06 17:58:51 -0400

    Updated Design

--- SwiftUIFormStyleProfile/ContentView.swift
+++ SwiftUIFormStyleProfile/ContentView.swift
@@ -9,52 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var subscriptions = ["Subscription 1", "Subscription 2", "Subscription 3"]
 
-    @State private var selectedSubscription = 0
-    
-    var transactions = ["Transaction 1", "Transaction 2", "Transaction 3"]
-
-    @State private var selectedTransactions = 0
     
     var body: some View {
-        NavigationView{
-            VStack{
-                Form{
+        NavigationView {
+            ScrollView {
+          
+                VStack {
                     
-                    Section{
-                        
-                        Picker(selection: $selectedSubscription, label: Text("Your Subscriptions")) {
-                            ForEach(0 ..< subscriptions.count) {
-                                Text(self.subscriptions[$0])
-                            }
-                        }
-                        
-                        Picker(selection: $selectedTransactions, label: Text("Your Transactions")) {
-                            ForEach(0 ..< transactions.count) {
-                                Text(self.transactions[$0])
-                            }
-                        }
+                    SettingsRow(imageName: "square.and.arrow.up", title: "Your Subscriptions") {
+                       
+                    }
+                
+                    SettingsRow(imageName: "pencil.and.outline", title: "Write a review") {
+         
+                    }
 
-                    }
                     
-                    Section{
-                   
-                        
+                    SettingsRow(imageName: "textbox", title: "Tweet about it") {
+                      
                     }
-                    
-                    Section{
-                        Button(action: {
-                           
-                        }) {
-                            Text("Sign Out")
-                          
-                        }
-                    }
-                   
                 }
-            }.navigationBarTitle("Profile")
-        }.navigationViewStyle(StackNavigationViewStyle())
+                .settingsBackground()
+
+           
+            }
+            .navigationBarTitle("Profile")
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
